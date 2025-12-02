ccf3e36b3f6d5ffaec3cd7b52a40b899c3d16aaa
koldun <anton.kitaev.97@gmail.com>
2025-12-02 10:08:23 +0300

    add movie controller

---
+++ src/controllers/movies.js
@@ -0,0 +1,79 @@
+import {movieService} from "../services/movies.js";
+
+export class MovieController {
+    constructor() {}
+
+    getMovies(req, res) {
+        const movies = movieService.getMovies();
+
+        res.status(200).json(movies);
+    }
+
+    postMovie(req, res) {
+        const movieData = req.body;
+
+        if(!movieData.genre || !movieData.title) {
+            res.status(400).send('incorrect movie data');
+            return;
+        }
+
+        const movieId = movieService.addMovie(movieData);
+
+        res.status(200).send(movieId);
+    }
+
+    getMovieById(req, res) {
+        const { id } = req.params;
+        if(!id) {
+            res.status(400).send('id not provided');
+            return;
+        }
+
+        const movie = movieService.getMovieById(id);
+
+        if (!movie) {
+            res.status(404).send('not found');
+            return;
+        }
+
+        res.status(200).json(movie);
+    }
+
+    updateMovie(req, res) {
+        const { id } = req.params;
+
+        const movieData = req.body;
+
+        if(!movieData.genre || !movieData.title || !movieData.rating) {
+            res.status(400).send({ message: 'incorrect movie data' });
+            return;
+        }
+
+        if(!movieService.updateMovie(id, movieData)) {
+            res.status(404).send({ message: 'not found' });
+        }
+
+        res.status(200).json({ message: 'OK' });
+    }
+
+    patchMovie(req, res) {
+        const { id } = req.params;
+
+        if(!movieService.updateMovie(id, req.body)) {
+            res.status(404).send({ message: 'not found' });
+        }
+
+        res.status(200).json({ message: 'OK' });
+    }
+
+    deleteMovie(req, res) {
+        const { id } = req.params;
+        if(!movieService.deleteMovie(id)) {
+            res.status(404).json({ message: 'OK' });
+        }
+
+        res.status(200).json({ message: 'OK' });
+    }
+}
+
+export const movieController = new MovieController();
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,79 +1,18 @@
 import { Router } from "express";
-import { movieService } from "../services/movies.js";
+import {movieController} from "../controllers/movies.js";
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-    const movies = movieService.getMovies();
+router.get("/", movieController.getMovies);
+router.get('/:id', movieController.getMovieById);
 
-    res.status(200).json(movies);
-});
+router.post('/', movieController.postMovie);
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    if(!id) {
-        res.status(400).send('id not provided');
-        return;
-    }
+router.put('/:id', movieController.updateMovie);
 
-    const movie = movieService.getMovieById(id);
+router.patch('/:id', movieController.patchMovie);
 
-    if (!movie) {
-        res.status(404).send('not found');
-        return;
-    }
-
-    res.status(200).json(movie);
-});
-
-router.post('/', (req, res) => {
-    const movieData = req.body;
-
-    if(!movieData.genre || !movieData.title) {
-        res.status(400).send('incorrect movie data');
-        return;
-    }
-
-    const movieId = movieService.addMovie(movieData);
-
-    res.status(200).send(movieId);
-});
-
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const movieData = req.body;
-
-    if(!movieData.genre || !movieData.title || !movieData.rating) {
-        res.status(400).send({ message: 'incorrect movie data' });
-        return;
-    }
-
-    if(!movieService.updateMovie(id, movieData)) {
-        res.status(404).send({ message: 'not found' });
-    }
-
-    res.status(200).json({ message: 'OK' });
-});
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-
-    if(!movieService.updateMovie(id, req.body)) {
-        res.status(404).send({ message: 'not found' });
-    }
-
-    res.status(200).json({ message: 'OK' });
-});
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    if(!movieService.deleteMovie(id)) {
-        res.status(404).json({ message: 'OK' });
-    }
-
-    res.status(200).json({ message: 'OK' });
-})
+router.delete('/:id', movieController.deleteMovie);
 
 export default router;
 
